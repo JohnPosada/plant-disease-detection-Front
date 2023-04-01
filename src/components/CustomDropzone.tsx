b75bf722b42d7551c53a6569d364d27b92ca5a13
JohnPosada <johnposada2000@gmail.com>
2023-03-31 20:40:38 -0500

    created dropzone component to upload image

---
+++ src/components/CustomDropzone.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { HiArrowSmDown } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+
+export const CustomDropzone = () => {
+  const [uploadFile, setUploadFile] = useState<File | {}>({});
+  const navigate = useNavigate();
+  const [previewImage, setPreviewImage] = useState({
+    preview: "",
+    path: "",
+  });
+
+  const onDrop = (acceptedFiles: File[]) => {
+    setUploadFile(acceptedFiles[0]);
+    setPreviewImage({
+      preview: URL.createObjectURL(acceptedFiles[0]),
+      path: acceptedFiles[0].name,
+    });
+  };
+  const { getRootProps, getInputProps, isDragActive, open, acceptedFiles } =
+    useDropzone({
+      onDrop,
+      noClick: true,
+      accept: {
+        "image/*": [".png", ".jpg"],
+      },
+      maxFiles: 1,
+    });
+
+  const onDragActive = () => {
+    return (
+      <>
+        <div className="h-screen w-screen fixed top-0 left-0 bg-black opacity-50"></div>
+        <HiArrowSmDown className="text-7xl font-black text-center text-H_white" />
+        <p className="text-xl font-black text-center text-H_white">
+          Drop the files here ...
+        </p>
+      </>
+    );
+  };
+
+  const onDragEmptyNotActive = () => {
+    return (
+      <>
+        <button
+          onClick={open}
+          className="bg-gray-400 py-1 px-8 rounded-lg text-lg shadow text-H_white"
+        >
+          Upload image
+        </button>
+
+        <p className="text-base text-center text-H_white">
+          Drag 'n' drop files here, or click in upload image to select file
+        </p>
+      </>
+    );
+  };
+
+  const onDragPreview = () => {
+    return (
+      <>
+        <img src={previewImage.preview} className="h-36" />
+        <p className="text-base text-center text-H_white">
+          {previewImage.path}
+        </p>
+        <button
+          onClick={open}
+          className="bg-gray-400 py-1 px-8 rounded-lg text-lg shadow text-H_white"
+        >
+          Change image...
+        </button>
+      </>
+    );
+  };
+
+  const onClickDiagnose = (file: File | {}) => {
+    console.log(file);
+    localStorage.setItem("imgURL", previewImage.preview);
+    navigate("/result");
+  };
+
+  return (
+    <div className="w-3/6 flex flex-col items-center gap-1">
+      <div
+        className="bg-H_coffee w-full h-72 rounded-xl my-8 p-2"
+        {...getRootProps()}
+      >
+        <div className="w-full h-full border-4 border-dashed border-spacing-40 border-H_gray flex flex-col justify-center items-center gap-1 rounded-md">
+          <input {...getInputProps()} />
+          {isDragActive && onDragActive()}
+          {!isDragActive &&
+            !!acceptedFiles.length === false &&
+            onDragEmptyNotActive()}
+          {!isDragActive && !!acceptedFiles.length && onDragPreview()}
+        </div>
+      </div>
+      <button
+        className={`bg-gray-200 group relative h-12 w-2/4 overflow-hidden rounded-lg text-lg shadow ${
+          !!acceptedFiles.length ? "block" : "hidden"
+        }`}
+        onClick={() => onClickDiagnose(uploadFile)}
+      >
+        <div className="absolute inset-0 w-3 bg-H_green transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+        <span className="relative text-black group-hover:text-white">
+          Diagnose
+        </span>
+      </button>
+    </div>
+  );
+};
