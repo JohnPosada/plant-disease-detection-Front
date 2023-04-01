6b1094aef9bf94137260ca8830573b4ff01fce8a
JohnPosada <johnposada2000@gmail.com>
2023-03-31 20:44:17 -0500

    edited InputText props

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -4,14 +4,15 @@ import { FC } from "react";
 interface Props {
   name: string;
   label: string;
-  classname?: string;
+  className?: string;
   type: string;
   placeholder: string;
+  hidden?: boolean;
 }
 
-export const InputText: FC<Props> = ({ ...props }) => {
+export const InputText: FC<Props> = ({ type, hidden, className, ...props }) => {
   const { name, label } = props;
-  const [field, meta] = useField(name);
+  const [field] = useField(name);
   return (
     <div>
       <label
@@ -22,6 +23,7 @@ export const InputText: FC<Props> = ({ ...props }) => {
       </label>
       <input
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        type={hidden ? "text" : type}
         {...field}
         {...props}
       />
