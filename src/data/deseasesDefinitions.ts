bdd4b9e29044ee945107a40cd8b4103c3def13c3
Alejandro Bernal <jbernalxyz@gmail.com>
2023-04-28 16:42:07 -0500

    fixed timeout, changed sickness names to lowercase, changed min percentage threshold

--- src/data/deseasesDefinitions.ts
+++ src/data/deseasesDefinitions.ts
@@ -1,91 +1,91 @@
 export const deseasesDefinitions = [
   {
-    name: "Apple_scab",
+    name: "apple_scab",
     description:
       "Apple scab is a common disease of plants in the rose family (Rosaceae) that is caused by the ascomycete fungus Venturia inaequalis. While this disease affects several plant genera, including Sorbus, Cotoneaster, and Pyrus, it is most commonly associated with the infection of Malus trees, including species of flowering crabapple, as well as cultivated apple. The first symptoms of this disease are found in the foliage, blossoms, and developing fruits of affected trees, which develop dark, irregularly-shaped lesions upon infection. Although apple scab rarely kills its host, infection typically leads to fruit deformation and premature leaf and fruit drop, which enhance the susceptibility of the host plant to abiotic stress and secondary infection. The reduction of fruit quality and yield may result in crop losses of up to 70%, posing a significant threat to the profitability of apple producers. To reduce scab-related yield losses, growers often combine preventive practices, including sanitation and resistance breeding, with reactive measures, such as targeted fungicide or biocontrol treatments, to prevent the incidence and spread of apple scab in their crops. ",
   },
   {
-    name: "Apple_black_rot",
+    name: "apple_black_rot",
     description:
       "Apple trees are amazing assets to the home landscape and orchard, but when things start to go wrong, it's often a fungus that's to blame. Black rot in apples is a common fungal disease that can spread from infected apple trees to other landscape plants, so it's important to watch your apple trees for signs of black rot disease in order to catch it early in the disease cycle. Distressing as it is, when block rot attacks your apple trees, it's not the end of the world. You can get your apples back and have healthy harvests if you understand how to destroy the disease",
   },
   {
-    name: "Apple_cedar_apple_rust",
+    name: "apple_cedar_apple_rust",
     description:
       "Cedar-apple rust and similar rusts are incited by several species of the fungal genus Gymnosporangium. Cedar-apple rust affects the health and vigor of apple trees since it causes premature defoliation and reduces fruit quality. If severe infections occur for several seasons, the result may be tree death. ",
   },
   {
-    name: "Apple_healthy",
+    name: "apple_healthy",
     description: "The apple leaf and fruit are healthy.",
   },
 
   {
-    name: "Background_without_leaves",
+    name: "background_without_leaves",
     description: "the photo is taken without leaves",
   },
   {
-    name: "Blueberry_healthy",
+    name: "blueberry_healthy",
     description: "the blueberry leaf and fruit are healthy",
   },
   {
-    name: "Cherry_powdery_mildew",
+    name: "cherry_powdery_mildew",
     description:
       "Powdery mildew of sweet and sour cherry is caused by Podosphaera clandestina, an obligate biotrophic fungus. Mid- and late-season sweet cherry (Prunus avium) cultivars are commonly affected, rendering them unmarketable due to the covering of white fungal growth on the cherry surface. Season long disease control of both leaves and fruit is critical to minimize overall disease pressure in the orchard and consequently to protect developing fruit from accumulating spores on their surfaces.",
   },
   {
-    name: "Cherry_healthy",
+    name: "cherry_healthy",
     description: "the cherry leaf and fruit are healthy",
   },
   {
-    name: "Corn_gray_leaf_spot",
+    name: "corn_gray_leaf_spot",
     description:
       "Gray leaf spot is a fungal disease that affects corn plants. It is caused by the fungus Cercospora zeae-maydis and is most commonly found in warm, humid climates. The disease is characterized by the appearance of grayish-brown lesions on the leaves of the corn plant. These lesions start out small and circular, but can grow up to several inches in length and become elongated with a characteristic 'cigar-shaped' appearance.",
   },
   {
-    name: "Corn_common_rust",
+    name: "corn_common_rust",
     description:
       "Common rust is a fungal disease that affects corn plants. It is caused by the fungus Puccinia sorghi and is one of the most common diseases of corn worldwide. The disease is characterized by the appearance of small, circular or oval-shaped pustules on the leaves, stems, and husks of the corn plant. These pustules are usually orange or reddish-brown in color, and can be surrounded by a yellow halo.",
   },
   {
-    name: "Corn_northern_leaf_blight",
+    name: "corn_northern_leaf_blight",
     description:
       "Northern leaf blight is a fungal disease that affects corn plants. It is caused by the fungus Exserohilum turcicum and is most commonly found in cool, humid climates. The disease is characterized by the appearance of long, cigar-shaped lesions on the leaves of the corn plant. These lesions are usually gray-green in color and can grow up to several inches in length.",
   },
   {
-    name: "Corn_healthy",
+    name: "corn_healthy",
     description: "the corn leaf are healthy",
   },
   {
-    name: "Grape_black_rot",
+    name: "grape_black_rot",
     description:
       "Black rot is a fungal disease that affects grapes. It is caused by the fungus Guignardia bidwellii and can have a significant impact on grape production. The disease is characterized by the appearance of small, circular lesions on the leaves, stems, and fruit of the grapevine. These lesions are usually reddish-brown in color and can have a dark border. As the disease progresses, the lesions can become more numerous and coalesce, causing large areas of the leaf to become necrotic and die.",
   },
   {
-    name: "Grape_black_measles",
+    name: "grape_black_measles",
     description:
       "Grape black measles is a fungal disease that affects grapes. It is caused by the fungus Phomopsis viticola and can have a significant impact on grape production. The disease is characterized by the appearance of small, dark spots or lesions on the leaves, stems, and fruit of the grapevine. These lesions can become larger and more numerous as the disease progresses.",
   },
   {
-    name: "Grape_leaf_blight",
+    name: "grape_leaf_blight",
     description:
       "Grape leaf blight is a fungal disease that affects grape leaves. It is caused by the fungus Phomopsis viticola and can have a significant impact on grape production. The disease is characterized by the appearance of small, circular lesions on the leaves of the grapevine. These lesions can become larger and more numerous as the disease progresses. As the disease progresses, the lesions can cause the leaves to turn yellow and drop prematurely. Severe infections can lead to reduced grape production and poor fruit quality. ",
   },
   {
-    name: "Grape_healthy",
+    name: "grape_healthy",
     description: "The grape leaf are healthy",
   },
   {
-    name: "Orange_haunglongbing",
+    name: "orange_haunglongbing",
     description:
       "Huanglongbing (HLB), also known as citrus greening disease, is a bacterial disease that affects citrus trees, including oranges. It is caused by the bacterium Candidatus Liberibacter asiaticus and is spread by the Asian citrus psyllid insect. The disease causes a number of symptoms, including yellowing and blotching of leaves, stunted growth, and premature drop of fruit. Fruit from infected trees are often small, misshapen, and have a bitter taste. The disease can ultimately lead to the death of the tree.",
   },
   {
-    name: "Peach_bacterial_spot",
+    name: "peach_bacterial_spot",
     description:
       "Peach bacterial spot is a disease caused by the bacterium Xanthomonas arboricola pv. pruni, which affects stone fruit trees such as peaches, nectarines, and plums. The disease can cause significant economic losses by reducing fruit yield and quality. The symptoms of peach bacterial spot include small, water-soaked spots on the leaves, which turn brown and necrotic as they age. On fruit, the disease causes small, raised, dark spots that become sunken as the fruit matures. Severe infections can lead to premature fruit drop.",
   },
   {
-    name: "Peach_healthy",
+    name: "peach_healthy",
     description: "The peach leaf and fruit are healthy",
   },
 ];
